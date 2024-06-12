658d6950456fc9a78c30ffc4690abf78e78427fe
Brandon Henry <itsbhenry@gmail.com>
2024-06-12 11:46:07 -0500

    updated resume

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,8 +20,7 @@ class Footer extends Component {
 
 
            <ul className="copyright">
-              <li>&copy; Copyright 2021 Brandon Henry</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2024 Brandon Henry</li>
            </ul>
 
         </div>
